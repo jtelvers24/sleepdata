d25428312408200299ba088e187703581ab180b0
John Elvers <jelvers@my.wctc.edu>
2022-01-31 13:53:18 -0600

    end date

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
-namespace sleepdata
+namespace SleepData
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+            if (resp == "1")
+            {
+                // create data file
+                 // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine());
+
+                 // determine start and end date
+                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                Console.WriteLine(dataDate);
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+            }
         }
     }
 }
