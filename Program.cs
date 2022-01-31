acdb4aeebf6989ed0e3c99ffdfc5152feedad759
John Elvers <jelvers@my.wctc.edu>
2022-01-31 13:45:33 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sleepdata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
