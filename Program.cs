73ac1310c3d2db94ee68f4df84ed30a61cfa4819
John Elvers <jelvers@my.wctc.edu>
2022-01-31 13:59:19 -0600

    write data to text file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+
 namespace SleepData
 {
     class Program
@@ -11,9 +13,11 @@ namespace SleepData
             Console.WriteLine("Enter anything else to quit.");
             // input response
             string resp = Console.ReadLine();
+
             if (resp == "1")
             {
                 // create data file
+
                  // ask a question
                 Console.WriteLine("How many weeks of data is needed?");
                 // input the response (convert to int)
@@ -26,10 +30,11 @@ namespace SleepData
                 // subtract # of weeks from endDate to get startDate
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
                 
-
                 // random number generator
                 Random rnd = new Random();
 
+                // create file
+                StreamWriter sw = new StreamWriter("data.txt");
                 // loop for the desired # of weeks
                 while (dataDate < dataEndDate)
                 {
@@ -41,14 +46,17 @@ namespace SleepData
                         hours[i] = rnd.Next(4, 13);
                     }
                     // M/d/yyyy,#|#|#|#|#|#|#
-                    Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
                     // add 1 week to date
                     dataDate = dataDate.AddDays(7);
                 }
+                sw.Close();
             }
             else if (resp == "2")
             {
                 // TODO: parse data file
+
             }
         }
     }
